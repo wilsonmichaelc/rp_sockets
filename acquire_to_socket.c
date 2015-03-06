ceacc65f87f19a32be26a197f7e6e7d28386664e
Michael Wilson <wilson.michael.c@gmail.com>
2015-03-06 16:45:41 -0500

    Working on continuous acquisition ... not sure how im going to handle the delay between rp_Acq, buffering, and writing to socket, but its a start

--- acquire_to_socket.c
+++ acquire_to_socket.c
@@ -18,26 +18,53 @@ void *acquire_to_socket(acq_params *parameters){
     rp_acq_decimation_t sample_rate = (rp_acq_decimation_t)params.sample_rate;
     rp_acq_trig_src_t trigger = (rp_acq_trig_src_t)params.trigger;
 
-    /* Create buffer and size to hold data points */
-    uint32_t array_size = 16 * 1024; 
-    float *buff;
-    buff = (float *)malloc(array_size * sizeof(float));
 
-    /* Starts acquisition */
-    rp_AcqStart();
-    rp_AcqSetTriggerSrc(trigger);
-    rp_AcqSetSamplingRate(sample_rate);
-    rp_AcqGetOldestDataV(channel, &array_size, buff);
-    
-    int i;
-    for (i = 0; i < array_size; ++i)
-    {
-        //send(sock, &buff[i], sizeof(float), 0);
-        char tmp[1024];
-        sprintf(tmp, "%f", buff[i]);
-        write(sock, tmp, strlen(tmp));
-        write(sock, "\n", strlen("\n"));
-    }   
+
+    if(num_points == 0){
+
+        /* Create buffer and size to hold data points */
+        uint32_t array_size = 16*1024; 
+        float *buff;
+        buff = (float *)malloc(array_size * sizeof(float));
+
+        /* Continuously acquire data */
+        while(1==1){
+            /* Starts acquisition */
+            rp_AcqStart();
+            rp_AcqSetTriggerSrc(trigger);
+            rp_AcqSetSamplingRate(sample_rate);
+            rp_AcqGetOldestDataV(channel, &array_size, buff);
+            
+            int i;
+            for (i = 0; i < array_size; ++i)
+            {
+                //send(sock, &buff[i], sizeof(float), 0);
+                char tmp[1024];
+                sprintf(tmp, "%f", buff[i]);
+                write(sock, tmp, strlen(tmp));
+                write(sock, "\n", strlen("\n"));
+            }
+        }
+
+    }else{
+
+        /* Starts acquisition */
+        rp_AcqStart();
+        rp_AcqSetTriggerSrc(trigger);
+        rp_AcqSetSamplingRate(sample_rate);
+        rp_AcqGetOldestDataV(channel, &array_size, buff);
+        
+        int i;
+        for (i = 0; i < array_size; ++i)
+        {
+            //send(sock, &buff[i], sizeof(float), 0);
+            char tmp[1024];
+            sprintf(tmp, "%f", buff[i]);
+            write(sock, tmp, strlen(tmp));
+            write(sock, "\n", strlen("\n"));
+        }
+
+    }
 
     /* Releasing resources */
     free(buff);
